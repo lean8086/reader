f568c562040a9ee327f8b8148d0be40430c75b28
lean8086 <leandro.linares@mercadolibre.com>
2019-09-28 19:14:52 +0200

    Move feeds to a module. Change the way to format dates

---
+++ feeds.mjs
@@ -0,0 +1,40 @@
+export default {
+  // TECH
+  'The Next Web': { domain: 'https://thenextweb.com', feed: 'https://feeds2.feedburner.com/thenextweb' },
+  'SpaceX': { domain: 'https://spacex.com', feed: 'https://www.spacex.com/news.xml/rss.xml' },
+  'Teslarati': { domain: 'https://teslarati.com', feed: 'https://www.teslarati.com/feed/', altFavicon: 'https://www.teslarati.com/wp-content/uploads/2015/10/cropped-Teslarati-iOS-White-Leaf-Icon-512x512-32x32.png' },
+  'Mashable': { domain: 'https://mashable.com', feed: 'http://feeds.mashable.com/Mashable' },
+  'Gizmodo': { domain: 'https://gizmodo.com', feed: 'https://gizmodo.com/rss', altFavicon: 'https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_80,q_80,w_80/fdj3buryz5nuzyf2k620.png' },
+  'WIRED': { domain: 'https://wired.com', feed: 'https://www.wired.com/feed/rss' },
+  'Everyday Astronaut': { domain: 'https://everydayastronaut.com', feed: 'https://everydayastronaut.com/feed/' },
+  'Ars Technica': { domain: 'https://arstechnica.com', feed: 'http://feeds.arstechnica.com/arstechnica/index/' },
+  'MIT Technology Review': { domain: 'https://technologyreview.com', feed: 'https://www.technologyreview.com/topnews.rss' },
+  'The Verge': { domain: 'https://theverge.com', feed: 'https://www.theverge.com/rss/index.xml' },
+  'ReadWrite': { domain: 'https://readwrite.com', feed: 'https://readwrite.com/feed/' },
+  'Engadget': { domain: 'https://engadget.com', feed: 'https://www.engadget.com/rss.xml', altFavicon: 'https://s.blogsmithmedia.com/www.engadget.com/assets-hd5b7ca125ee4d9a2872d820a7713960b/images/favicon-32x32.png' },
+  // GAMING
+  'Polygon': { domain: 'https://www.polygon.com', feed: 'https://www.polygon.com/rss/index.xml' },
+  'Kotaku': { domain: 'https://kotaku.com', feed: 'https://kotaku.com/rss', altFavicon: 'https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_80,q_80,w_80/v4sckews2f3bzf0ztbkf.png' },
+  'GameSpot': { domain: 'https://gamespot.com', feed: 'https://www.gamespot.com/feeds/mashup/' },
+  'Unity3d': { domain: 'https://unity3d.com', feed: 'https://blogs.unity3d.com/feed/' },
+  'IGN Germany': { domain: 'https://ign.com', feed: 'https://de.ign.com/feed.xml' },
+  // MAKER
+  'Prusa3D': { domain: 'https://www.prusa3d.com', feed: 'https://www.prusa3d.com/feed/' },
+  'Prusa Printers': { domain: 'https://blog.prusaprinters.org', feed: 'https://blog.prusaprinters.org/feed/' },
+  'Makezine': { domain: 'https://makezine.com', feed: 'https://makezine.com/feed/' },
+  'Arduino': { domain: 'https://blog.arduino.cc', feed: 'https://blog.arduino.cc/feed/' },
+  'Raspberry Pi': { domain: 'https://www.raspberrypi.org/blog/', feed: 'https://www.raspberrypi.org/feed/' },
+  'The MagPi Magazine': { domain: 'https://www.raspberrypi.org/magpi/', feed: 'https://www.raspberrypi.org/magpi/feed/' },
+  'Wireframe Magazine': { domain: 'https://wireframe.raspberrypi.org', feed: 'https://wireframe.raspberrypi.org/features' },
+  'Tested': { domain: 'https://www.tested.com', feed: 'https://www.tested.com/feeds/' },
+  'All3DP': { domain: 'https://all3dp.com', feed: 'https://all3dp.com/feed/' },
+  'Adafruit': { domain: 'https://www.adafruit.com', feed: 'http://blog.adafruit.com/feed/' },
+  'Cults': { domain: 'https://cults3d.com', feed: 'https://cults3d.com/en/creations/latest.rss' },
+  // CODE
+  'Human Who Codes': { domain: 'https://humanwhocodes.com', feed: 'https://humanwhocodes.com/feeds/blog.xml' },
+  'Overreacted': { domain: 'https://overreacted.io', feed: 'https://overreacted.io/rss.xml' },
+  'Smashing Magazine': { domain: 'https://www.smashingmagazine.com', feed: 'https://www.smashingmagazine.com/feed/' },
+  'CSS-Tricks': { domain: 'https://css-tricks.com', feed: 'http://feeds.feedburner.com/CssTricks' },
+  'DEV Community': { domain: 'https://dev.to', feed: 'https://dev.to/feed' },
+};
+// console.log(`'${document.title}': { domain: '${location.origin}', feed: '${document.head.querySelector('[type="application/rss+xml"]').href}' },`)
