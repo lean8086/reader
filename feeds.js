be2ee1b0a3582f87f39fc826b8ddc5bcf197ca6e
lean8086 <leandro.linares@mercadolibre.com>
2019-10-13 01:13:53 +0200

    Big css restruct

--- feeds.js
+++ feeds.js
@@ -31,16 +31,16 @@ export const feedList = {
 };
 
 export const feedCategs = {
-  '🚀 Technology': {
+  'Technology': {
     feeds: '0,1,2,3,4,5,6,7,8,9',
   },
-  '🕹 Gaming': {
+  'Gaming': {
     feeds: '10,11,12,13,14,15',
   },
-  '🛠 Makers': {
+  'Makers': {
     feeds: '16,17,18,19,20,21',
   },
-  '🖥 Coding': {
+  'Coding': {
     feeds: '22,23,24',
   },
 };
