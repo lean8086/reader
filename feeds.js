fb9fe103ac41a9b2ac4fe442dc61accf8e0f3c8c
lean8086 <leandro.linares@mercadolibre.com>
2019-10-15 22:08:52 +0200

    Rearrange feeds

--- feeds.js
+++ feeds.js
@@ -1,5 +1,4 @@
 export const feedList = {
-  // TECH
   'The Next Web': { domain: 'https://thenextweb.com', feed: 'https://feeds2.feedburner.com/thenextweb' },
   'Teslarati': { domain: 'https://teslarati.com', feed: 'https://www.teslarati.com/feed/', altFavicon: 'https://www.teslarati.com/wp-content/uploads/2015/10/cropped-Teslarati-iOS-White-Leaf-Icon-512x512-32x32.png' },
   'Mashable': { domain: 'https://mashable.com', feed: 'http://feeds.mashable.com/Mashable' },
@@ -10,24 +9,23 @@ export const feedList = {
   'The Verge': { domain: 'https://theverge.com', feed: 'https://www.theverge.com/rss/index.xml', altFavicon: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/7395363/favicon-32x32.0.png' },
   'ReadWrite': { domain: 'https://readwrite.com', feed: 'https://readwrite.com/feed/', altFavicon: 'https://images.readwrite.com/wp-content/uploads/2018/03/cropped-rw-32x32.jpg' },
   'Engadget': { domain: 'https://engadget.com', feed: 'https://www.engadget.com/rss.xml' },
-  // GAMING
   'Polygon': { domain: 'https://www.polygon.com', feed: 'https://www.polygon.com/rss/index.xml', altFavicon: 'https://cdn.vox-cdn.com/community_logos/51927/38.png' },
   'GameSpot': { domain: 'https://gamespot.com', feed: 'https://www.gamespot.com/feeds/mashup/' },
   'Unity3d': { domain: 'https://unity3d.com', feed: 'https://blogs.unity3d.com/feed/', altFavicon: 'https://unity.com/themes/contrib/unity_base/images/favicons/apple-touch-icon-72x72.png' },
   'IGN': { domain: 'https://ign.com', feed: 'http://feeds.ign.com/ign/all' },
   'Gamasutra': { domain: 'https://gamasutra.com/', feed: 'http://feeds.feedburner.com/GamasutraNews' },
   'Gamesindustry': { domain: 'https://www.gamesindustry.biz/', feed: 'https://www.gamesindustry.biz/rss/gamesindustry_news_feed.rss' },
-  // MAKER
   'Prusa3D': { domain: 'https://www.prusa3d.com', feed: 'https://www.prusa3d.com/feed/', altFavicon: 'https://www.prusa3d.com/wp-content/uploads/2014/10/prusa_fav_72x72.png' },
   'Makezine': { domain: 'https://makezine.com', feed: 'https://makezine.com/feed/' },
   'Arduino': { domain: 'https://blog.arduino.cc', feed: 'https://blog.arduino.cc/feed/' },
   'Raspberry Pi': { domain: 'https://www.raspberrypi.org/blog/', feed: 'https://www.raspberrypi.org/feed/', altFavicon: 'https://www.raspberrypi.org/app/themes/mind-control/images/favicon.png' },
   'The MagPi Magazine': { domain: 'https://www.raspberrypi.org/magpi/', feed: 'https://www.raspberrypi.org/magpi/feed/', altFavicon: 'https://www.raspberrypi.org/app/themes/mind-control/images/favicon.png' },
   'Tested': { domain: 'https://www.tested.com', feed: 'https://www.tested.com/feeds/', altFavicon: 'https://d2rormqr1qwzpz.cloudfront.net/static/tested_favicon_32.png?v=3' },
-  // CODE
   'Smashing Magazine': { domain: 'https://www.smashingmagazine.com', feed: 'https://www.smashingmagazine.com/feed/', altFavicon: 'https://www.smashingmagazine.com/images/favicon/favicon.png' },
   'CSS-Tricks': { domain: 'https://css-tricks.com', feed: 'http://feeds.feedburner.com/CssTricks' },
   'DEV Community': { domain: 'https://dev.to', feed: 'https://dev.to/feed' },
+  'Harvard Business Review' : { domain: 'https://hbr.org/', feed: 'http://feeds.hbr.org/harvardbusiness' },
+  'The Planetary Society': { domain: 'http://www.planetary.org/', feed: 'http://www.planetary.org/system/rss/channel.jsp?feedID=328434498' },
 };
 
 export const feedCategs = {
@@ -35,12 +33,18 @@ export const feedCategs = {
     feeds: '0,1,2,3,4,5,6,7,8,9',
   },
   'Gaming': {
-    feeds: '10,11,12,13,14,15',
+    feeds: '10,11,13,14,15',
   },
   'Makers': {
     feeds: '16,17,18,19,20,21',
   },
   'Coding': {
-    feeds: '22,23,24',
+    feeds: '12,22,23,24',
+  },
+  'Business': {
+    feeds: '25',
+  },
+  'Space': {
+    feeds: '26',
   },
 };
